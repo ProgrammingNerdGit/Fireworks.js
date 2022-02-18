b9d6f267a09d82232d726c931e889205d3ba81da
ThreshMain <martinkos007@gmail.com>
2022-02-18 09:10:49 +0100

    Added time to live param to rockets

--- js/rocket.js
+++ js/rocket.js
@@ -1,7 +1,9 @@
 const max_speed = 4;
 const min_speed = 1;
-const default_alpha = 0.9;
+const default_alpha = 0.8;
 const frame_time_divider = 8;
+const alpha_decrease_start = 40;
+const time_to_live = 80;
 
 function createVector(x, y) {
     return {
@@ -16,7 +18,6 @@ class Color {
         this.b = b;
         this.alpha = alpha;
     }
-
 
     toRGBString(alpha = this.alpha) {
         return `rgba(${this.r}, ${this.g}, ${this.b}, ${alpha})`;
@@ -39,7 +40,8 @@ class Rocket {
     constructor(x, y, context, color = random_color()) {
         this.position = createVector(x, y);
         this.velocity = createVector(random_number(max_speed, min_speed), random_number(max_speed, min_speed));
-        this.radius = 120;
+        this.radius = 340;
+        this.time_to_live = Math.random() * time_to_live + time_to_live;
         this.alive = true;
         this.context = context;
         this.color = color;
@@ -52,21 +54,26 @@ class Rocket {
         const step = 1 / this.radius;
         let i = 0;
 
-        let h = this.radius / 2, hSqr = h * h;
-        h = this.gradient_seed;
+        let hSqr = this.radius ** 2;
+        let h = this.gradient_seed;
+        let alpha_decrease = 0;
+        if (this.time_to_live < alpha_decrease_start) {
+            alpha_decrease = (alpha_decrease_start - this.time_to_live) / alpha_decrease_start * this.color.alpha;
+        }
         while (i < 1) {
             const distSqr = (i * this.radius) ** 2 + hSqr;
-            const ref = (h / distSqr ** 0.5) / distSqr;
-            this.gradient.addColorStop(i, hexCol(this.color.r, this.color.g, this.color.b, 256 * hSqr * ref * (1 - i)));
+            const ref = (h / distSqr ** 0.6) / distSqr;
+            this.gradient.addColorStop(i, hexCol(this.color.r, this.color.g, this.color.b, (this.color.alpha - alpha_decrease) * 255 * hSqr * ref * (1 - i)));
             i += step;
         }
         this.gradient.addColorStop(1, "#0000");
     }
 
     update(context, update_speed) {
+        this.time_to_live -= update_speed / frame_time_divider;
         this.position.x += this.velocity.x * update_speed / frame_time_divider;
         this.position.y += this.velocity.y * update_speed / frame_time_divider;
-        this.alive = !(this.position.x > 0 && this.position.x < context.width && this.position.y > 0 && this.position.y < context.height);
+        this.alive = !(this.time_to_live < 0 || this.position.x < 0 || this.position.x > context.width || this.position.y < 0 || this.position.y > context.height);
         this.calculate_gradient();
     }
 
