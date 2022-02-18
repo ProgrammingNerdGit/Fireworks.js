a7b580f7bb18f5cfbe4d65821b1ceb61b27de9d4
ThreshMain <martinkos007@gmail.com>
2022-02-18 10:42:34 +0100

    Cache gradient generation seed for better performance

--- js/rocket.js
+++ js/rocket.js
@@ -1,9 +1,14 @@
 const max_speed = 4;
 const min_speed = 1;
-const default_alpha = 0.8;
+const default_alpha = 1;
 const frame_time_divider = 8;
 const alpha_decrease_start = 40;
 const time_to_live = 80;
+
+const explode_chance = 0.125;
+
+const explode_count = 20;
+const explode_time_to_live = 20;
 
 function createVector(x, y) {
     return {
@@ -33,15 +38,17 @@ function random_number(max_speed, min_speed) {
 }
 
 function random_color() {
-    return colors[Math.floor(Math.random() * colors.length)];
+    let color = colors[Math.floor(Math.random() * colors.length)]
+    return new Color(color.r, color.g, color.b, color.alpha);
 }
 
 class Rocket {
     constructor(x, y, context, color = random_color()) {
         this.position = createVector(x, y);
         this.velocity = createVector(random_number(max_speed, min_speed), random_number(max_speed, min_speed));
-        this.radius = 340;
+        this.radius = 120;
         this.time_to_live = Math.random() * time_to_live + time_to_live;
+        this.explode = Math.random() < explode_chance;
         this.alive = true;
         this.context = context;
         this.color = color;
@@ -50,31 +57,78 @@ class Rocket {
     }
 
     calculate_gradient() {
-        this.gradient = this.context.createRadialGradient(this.position.x, this.position.y, 0, this.position.x, this.position.y, this.radius);
+        this.gradient_values = [];
         const step = 1 / this.radius;
         let i = 0;
 
         let hSqr = this.radius ** 2;
         let h = this.gradient_seed;
         let alpha_decrease = 0;
-        if (this.time_to_live < alpha_decrease_start) {
+        if (this.time_to_live < alpha_decrease_start && !this.explode) {
             alpha_decrease = (alpha_decrease_start - this.time_to_live) / alpha_decrease_start * this.color.alpha;
         }
         while (i < 1) {
             const distSqr = (i * this.radius) ** 2 + hSqr;
             const ref = (h / distSqr ** 0.6) / distSqr;
-            this.gradient.addColorStop(i, hexCol(this.color.r, this.color.g, this.color.b, (this.color.alpha - alpha_decrease) * 255 * hSqr * ref * (1 - i)));
+            this.gradient_values.push({
+                value: i,
+                color: hexCol(this.color.r, this.color.g, this.color.b, (this.color.alpha - alpha_decrease) * 255 * hSqr * ref * (1 - i))
+            });
             i += step;
         }
-        this.gradient.addColorStop(1, "#0000");
+        this.gradient_values.push({
+            value: 1,
+            color: "#0000"
+        });
+        this.update_gradient(context);
+    }
+
+    update_gradient(){
+        this.gradient = this.context.createRadialGradient(this.position.x, this.position.y, 0, this.position.x, this.position.y, this.radius);
+        this.gradient_values.forEach(value => {
+            this.gradient.addColorStop(value.value, value.color);
+        });
+    }
+
+    explosion_rocket(){
+        let rocket = new Rocket(this.position.x, this.position.y, context);
+        rocket.radius = this.radius / 5;
+        rocket.time_to_live = Math.random() * explode_time_to_live + explode_time_to_live;
+        rocket.explode = 0;
+        rocket.color.alpha = default_alpha / 15;
+        return rocket;
+    }
+    explosion() {
+        let current_explode_count = explode_count + Math.floor(Math.random() * explode_count);
+
+        let explosion_rocket = this.explosion_rocket();
+        explosion_rocket.velocity = createVector(0, 0);
+        explosion_rocket.radius = this.radius * 5;
+        explosion_rocket.color = new Color(255, 255, 255, default_alpha*3)
+        explosion_rocket.time_to_live = Math.random() * explode_time_to_live + explode_time_to_live*2;
+        explosion_rocket.calculate_gradient();
+        rockets.push(explosion_rocket);
+
+        for (let i = 0; i < current_explode_count; i++) {
+            let angle = (Math.PI * 2 * ((current_explode_count - i) / current_explode_count)) + Math.random() * 0.1;
+            let speed_x = min_speed * 1.5 * Math.cos(angle);
+            let speed_y = min_speed * 1.5 * Math.sin(angle);
+            let rocket = this.explosion_rocket();
+            rocket.velocity = createVector(speed_x, speed_y);
+            rockets.push(rocket);
+        }
+        this.alive = false;
     }
 
     update(context, update_speed) {
         this.time_to_live -= update_speed / frame_time_divider;
         this.position.x += this.velocity.x * update_speed / frame_time_divider;
         this.position.y += this.velocity.y * update_speed / frame_time_divider;
+        if (this.alive && this.time_to_live < 0 && this.explode) {
+            this.explosion();
+        }
         this.alive = !(this.time_to_live < 0 || this.position.x < 0 || this.position.x > context.width || this.position.y < 0 || this.position.y > context.height);
-        this.calculate_gradient();
+        this.update_gradient(context);
     }
 
     draw(context) {
